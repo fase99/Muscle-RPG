45ca63d902a034ba36ef531ebfe35d5b03ff03ca
felipe <pipemendezri@gmail.com>
2025-12-10 15:51:33 -0300

    Update app.module.ts

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './users/users.module';
+import { RutinasModule } from './rutinas/rutinas.module';
 
 @Module({
-  imports: [],
-
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.MONGODB_URI || 'mongodb://admin:muscleRPG2025@localhost:27017/muscle_rpg?authSource=admin'),
+    UsersModule,
+    RutinasModule,
+  ],
 })
 export class AppModule {}
