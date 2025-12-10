37c8c183e88172e4db5be5eb17565f9b4d918cfc
felipe <pipemendezri@gmail.com>
2025-12-10 15:55:55 -0300

    Update rutinas.controller.ts

--- backend/src/rutinas/rutinas.controller.ts
+++ backend/src/rutinas/rutinas.controller.ts
@@ -3,55 +3,55 @@ import { RutinasService } from './rutinas.service';
 
 @Controller('rutinas')
 export class RutinasController {
-  constructor(private readonly rutinasService: RutinasService) {}
+    constructor(private readonly rutinasService: RutinasService) { }
 
-  @Post()
-  @HttpCode(HttpStatus.CREATED)
-  create(@Body() createRutinaDto: any) {
-    return this.rutinasService.create(createRutinaDto);
-  }
+    @Post()
+    @HttpCode(HttpStatus.CREATED)
+    create(@Body() createRutinaDto: any) {
+        return this.rutinasService.create(createRutinaDto);
+    }
 
-  @Get()
-  findAll() {
-    return this.rutinasService.findAll();
-  }
+    @Get()
+    findAll() {
+        return this.rutinasService.findAll();
+    }
 
-  @Get('user/:usuarioId')
-  findByUser(@Param('usuarioId') usuarioId: string) {
-    return this.rutinasService.findByUser(usuarioId);
-  }
+    @Get('user/:usuarioId')
+    findByUser(@Param('usuarioId') usuarioId: string) {
+        return this.rutinasService.findByUser(usuarioId);
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.rutinasService.findOne(id);
-  }
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.rutinasService.findOne(id);
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRutinaDto: any) {
-    return this.rutinasService.update(id, updateRutinaDto);
-  }
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() updateRutinaDto: any) {
+        return this.rutinasService.update(id, updateRutinaDto);
+    }
 
-  @Patch(':id/ejercicio/:index')
-  marcarEjercicioCompletado(
-    @Param('id') id: string,
-    @Param('index') index: string,
-    @Body() body: { completado: boolean },
-  ) {
-    return this.rutinasService.marcarEjercicioCompletado(
-      id,
-      parseInt(index),
-      body.completado,
-    );
-  }
+    @Patch(':id/ejercicio/:index')
+    marcarEjercicioCompletado(
+        @Param('id') id: string,
+        @Param('index') index: string,
+        @Body() body: { completado: boolean },
+    ) {
+        return this.rutinasService.marcarEjercicioCompletado(
+            id,
+            parseInt(index),
+            body.completado,
+        );
+    }
 
-  @Patch(':id/completar')
-  marcarRutinaCompletada(@Param('id') id: string) {
-    return this.rutinasService.marcarRutinaCompletada(id);
-  }
+    @Patch(':id/completar')
+    marcarRutinaCompletada(@Param('id') id: string) {
+        return this.rutinasService.marcarRutinaCompletada(id);
+    }
 
-  @Delete(':id')
-  @HttpCode(HttpStatus.NO_CONTENT)
-  remove(@Param('id') id: string) {
-    return this.rutinasService.remove(id);
-  }
+    @Delete(':id')
+    @HttpCode(HttpStatus.NO_CONTENT)
+    remove(@Param('id') id: string) {
+        return this.rutinasService.remove(id);
+    }
 }
