772d7b492d66d1987dfc25914b209845d294bf9c
felipe <pipemendezri@gmail.com>
2025-12-11 02:39:09 -0300

    Update app.routes.ts

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,23 +1,31 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './auth/auth.guard';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'login',
     pathMatch: 'full'
   },
   {
+    path: 'login',
+    loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)
+  },
+  {
     path: 'ejercicios',
-    loadComponent: () => import('./Ejercicios/ejercicios.component').then(m => m.EjerciciosComponent)
+    loadComponent: () => import('./Ejercicios/ejercicios.component').then(m => m.EjerciciosComponent),
+    canActivate: [authGuard]
   },
   {
     path: 'home',
-    loadComponent: () => import('./home/home').then(m =>m.Home)
+    loadComponent: () => import('./home/home').then(m =>m.Home),
+    canActivate: [authGuard]
   },
   {
   path: 'rutina',
   loadComponent: () =>
-    import('./rutina/rutina.component').then(m => m.RutinaComponent)
+    import('./rutina/rutina.component').then(m => m.RutinaComponent),
+    canActivate: [authGuard]
   },
   {
     path: 'perfil',
