c7aa478b2d0977aac07e765f2ee767d57858bd9a
felipe <pipemendezri@gmail.com>
2025-12-11 17:31:51 -0300

    Update home.ts

--- frontend/src/app/home/home.ts
+++ frontend/src/app/home/home.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+import { UserFromDB } from '../services/user-http.service';
 
 @Component({
   selector: 'app-home',
@@ -8,24 +10,42 @@ import { RouterLink } from '@angular/router';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {
+export class Home implements OnInit {
+  user: UserFromDB | null = null;
+
+  constructor(public authService: AuthService) {}
+
+  ngOnInit() {
+    this.authService.currentUser$.subscribe(user => {
+      this.user = user;
+    });
+  }
+
+  get userName(): string {
+    return this.user?.nombre || 'Invitado';
+  }
+
   // User Stats
-  userStats = {
-    level: 5,
-    currentXP: 1200,
-    maxXP: 2000,
-    get xpPercentage() {
-      return (this.currentXP / this.maxXP) * 100;
-    },
-    get xpToNextLevel() {
-      return this.maxXP - this.currentXP;
-    },
-    streak: 7,
-    totalWorkouts: 45,
-    totalMinutes: 2340,
-    personalRecords: 12,
-    goalsCompleted: 8
-  };
+  get userStats() {
+    const currentXP = this.user?.experiencia || 0;
+    const maxXP = this.user?.experienciaMaxima || 100;
+    return {
+      level: this.user?.nivel || 1,
+      currentXP,
+      maxXP,
+      get xpPercentage() {
+        return (currentXP / maxXP) * 100;
+      },
+      get xpToNextLevel() {
+        return maxXP - currentXP;
+      },
+      streak: this.user?.rachasDias || 0,
+      totalWorkouts: 45,
+      totalMinutes: 2340,
+      personalRecords: 12,
+      goalsCompleted: this.user?.logrosObtenidos || 0
+    };
+  }
 
   // Today's Workout
   todayWorkout = [
